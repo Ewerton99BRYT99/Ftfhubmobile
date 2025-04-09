90d12185f609b809b20a9202b4c39fccc0d529ac
Ewerton99BRYT99 <ewerton99backs@gmail.com>
2025-04-09 17:30:43 -0300

    Update Ftfhubmobilesource.lua

--- Ftfhubmobilesource.lua
+++ Ftfhubmobilesource.lua
@@ -88,11 +88,9 @@ Section2:NewToggle("TracersESP", "ToggleInfo", function(state)
     if state then
         getgenv().ESPTracers =  true
         TracersESP()
-	AssignTracers()
     else
         getgenv().ESPTracers = false
         TracersESP()
-	AssignTracers()
     end
 end)
 Section2:NewToggle("PCEsp", "ToggleInfo", function(state)
